6aa67dcb508cb656fd6f5ecd7b949e3769518ca4
enjoysea <779630561@qq.com>
2019-04-01 17:34:17 +0800

    路由跳转到默认的二级页面的两种方法

---
+++ 1-掏沙得金/53-路由跳转到默认的二级页面的两种方法/方法一/nav.js
@@ -0,0 +1,57 @@
+const Home = () => {
+  return import('@/views/home/home')
+}
+
+const Look = () => import('@/views/look/look')
+const Message = () => /* webpackChunkName: "class" */ import('@/views/message/message')
+
+const Pinglun = () => /* webpackChunkName: "class" */ import('@/views/message/children/pinglun')
+const Jianxin = () => /* webpackChunkName: "class" */ import('@/views/message/children/jianxin')
+const Guanzhu = () => /* webpackChunkName: "class" */ import('@/views/message/children/guanzhu')
+
+// 如果多可以将其拎到另外一个文件
+// message的二级导航
+export var messageChild = [
+  {
+    path: '',
+    name: 'Pinglun', // 路径为空，name要加在子路由上
+    title: '评论',
+    component: Pinglun
+  },
+  {
+    path: 'jianxin',
+    name: 'Jianxin',
+    title: '简信',
+    component: Jianxin
+  },
+  {
+    path: 'guanzhu',
+    name: 'Guanzhu',
+    title: '关注',
+    component: Guanzhu
+  }
+]
+
+export const navData = [
+  {
+    path: '/',
+    name: 'Home',
+    title: '发现',
+    component: Home
+  },
+  {
+    path: '/look',
+    name: 'Look',
+    title: '关注',
+    component: Look
+  },
+  {
+    path: '/message',
+    // name: 'Message', // 当子路由的path为''，name不能加在父路由上
+    title: '消息',
+    component: Message,
+    children:[
+      ...messageChild
+    ]
+  }
+]
